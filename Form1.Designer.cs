077c8e24d955eb34ee4fd508bc2148292301153b
Junior Figuereo <juniorsj@live.com>
2020-08-08 17:14:10 -0400

    progress

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,12 +35,13 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(651, 379);
+            this.button1.Location = new System.Drawing.Point(891, 563);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(117, 42);
             this.button1.TabIndex = 0;
@@ -52,7 +53,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(296, 187);
+            this.label1.Location = new System.Drawing.Point(391, 264);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(98, 25);
             this.label1.TabIndex = 1;
@@ -61,9 +62,10 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(413, 187);
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(508, 264);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(117, 20);
+            this.textBox1.Size = new System.Drawing.Size(212, 31);
             this.textBox1.TabIndex = 3;
             this.textBox1.UseSystemPasswordChar = true;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
@@ -71,7 +73,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(413, 223);
+            this.button2.Location = new System.Drawing.Point(508, 342);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(117, 42);
             this.button2.TabIndex = 5;
@@ -82,7 +84,7 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(74, 47);
+            this.button3.Location = new System.Drawing.Point(231, 49);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(109, 41);
             this.button3.TabIndex = 6;
@@ -93,7 +95,7 @@
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(74, 223);
+            this.button5.Location = new System.Drawing.Point(231, 494);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(109, 41);
             this.button5.TabIndex = 8;
@@ -104,7 +106,7 @@
             // button6
             // 
             this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(74, 137);
+            this.button6.Location = new System.Drawing.Point(231, 315);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(109, 41);
             this.button6.TabIndex = 9;
@@ -113,11 +115,22 @@
             this.button6.Visible = false;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(396, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(306, 40);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.Visible = false;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(801, 443);
+            this.ClientSize = new System.Drawing.Size(1054, 637);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button3);
@@ -143,6 +156,7 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,35 @@ namespace AccountStorage
             button1.Text = "Close";  //when form loads.. do these properties
         }
 
+        public static class Globals
+        {
+            public static readonly String path = @"C:\Users\Junio\source\repos\UIStorage\data.txt";
+            public static List<Creds> ListOfCreds = new List<Creds>();
+
+            public static FileStream fileStream = null;
+            public static bool fileOpened = false;
+
+            public static string userinput = "";
+
+
+            /* Notes
+            public const Int32 BUFFER_SIZE = 512; // Unmodifiable
+            public static String FILE_NAME = "Output.txt"; // Modifiable
+            public static readonly String CODE_PREFIX = "US-"; // Unmodifiable
+
+            in another class calling the global variables:
+            String code = Globals.CODE_PREFIX + value.ToString();
+            */
+        }
+
+        public class Creds
+        {
+            public string User { get; set; }
+            public string Pass { get; set; }
+            public string Secondary { get; set; }
+        }
+
+        //close button
         private void button1_Click(object sender, EventArgs e)
         {
             //MessageBox.Show("Thanks for clicking."); if button is clicked; message box.
@@ -32,11 +62,16 @@ namespace AccountStorage
                             MessageBoxButtons.YesNo, MessageBoxIcon.Question);
 
             if(dr == DialogResult.Yes)
-                this.Close();
+            {
+                if (Globals.fileOpened)
+                    Globals.fileStream.Close();
 
-           
+                this.Close();
+            }
+ 
         }
 
+        //password text
         private void label1_Click(object sender, EventArgs e)
         {
             // to make something clickable
@@ -44,11 +79,13 @@ namespace AccountStorage
             // use groupboxes for radio buttons
         }
 
+        //password entry
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
 
+        //submit button
         private void button2_Click(object sender, EventArgs e)
         {
             string password = textBox1.Text;
@@ -61,14 +98,33 @@ namespace AccountStorage
                 button3.Visible = true;
                 button5.Visible = true;
                 button6.Visible = true;
+                textBox2.Visible = true;
+                
+                Globals.fileStream = File.Open(Globals.path, File.Exists(Globals.path) ? FileMode.Append : FileMode.OpenOrCreate);
+                Globals.fileOpened = true;
             }
             else
                 MessageBox.Show("Incorrect");
+
         }
 
+        //add button
+        private void button3_Click(object sender, System.EventArgs e)
+        {
+            Globals.userinput = textBox2.Text;
+            MessageBox.Show(Globals.userinput);
+        }
+
+        //delete button
         private void button6_Click(object sender, EventArgs e)
         {
 
+        }
+
+        //adding list item textbox
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+           
         }
     }
 }
